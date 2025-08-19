320e9e08163a0115e1f69ef582dfbb85aafc3648
Shreyash Vishwasrao <shreyashv0929@gmail.com>
2025-08-19 07:26:27 +0530

    Add Messaging Layout

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -15,7 +15,7 @@ export const useChatStore = create((set) => ({
       const response = await axiosInstance.get("/message/users");
       set({ users: response.data.message });
     } catch (error) {
-      console.log("Error in getUsers: ", error);
+      console.error("Error in getUsers: ", error);
       toast.error(error.response.data.message);
     } finally {
       set({ isUsersLoading: false });
@@ -28,10 +28,27 @@ export const useChatStore = create((set) => ({
       const response = await axiosInstance.get(`/message/${userId}`);
       set({ messages: response.data.message });
     } catch (error) {
-      console.log("Error in getUsers: ", error);
+      console.error("Error in getUsers: ", error);
       toast.error(error.response.data.message);
     } finally {
       set({ isMessagesLoading: false });
+    }
+  },
+
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    try {
+      const response = await axiosInstance.post(
+        `/message/send/${selectedUser._id}`,
+        messageData,
+        {
+          headers: { "Content-Type": "multipart/form-data" },
+        }
+      );
+      set({ messages: [...messages, response.data.message] });
+    } catch (error) {
+      console.error("Error in getUsers: ", error);
+      toast.error(error.response.data.message);
     }
   },
 
